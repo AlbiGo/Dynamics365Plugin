e991b4ae3570de057fb32e632b2af61db64c2cd9
Albi Goxhaj <albi.goxhaj@moreone.tech>
2020-02-11 15:01:32 +0100

    Add project files.

---
+++ CRMDev/Helper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace CRMDev
+{
+    class Helper
+    {
+        public  Entity getCategory(string categoryName , IOrganizationService service)
+        {
+            var Account_query = new QueryExpression("new_category") { ColumnSet = new ColumnSet(true) };//Select * from tbl_name
+            #region Account Query
+            Account_query.Criteria.AddCondition(new ConditionExpression("new_name", ConditionOperator.Equal, categoryName));
+            Account_query.Criteria.FilterOperator = LogicalOperator.And;
+            #endregion
+            Entity category = service.RetrieveMultiple(Account_query).Entities[0];
+            return category;
+        }
+    }
+}
---
+++ CRMDev/ProjectBudget.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRMDev
+{
+   
+    public class ProjectBudget : IPlugin
+    {
+        public string getSelectedValue(int selectedValue)
+        {
+            string category = "";
+            switch (selectedValue)
+            {
+                case 100000000:
+                    category = "IT";
+                    break;
+                case 100000001:
+                    category = "Marketing";
+                    break;
+                case 100000002:
+                    category = "Finance";
+                    break;
+                case 100000003:
+                    category = "Social";
+                    break;
+                case 100000004:
+                    category = "Office Supply";
+                    break;
+                default:
+                    break;
+
+            }
+            return category;
+        }
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            IOrganizationServiceFactory factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+
+            IOrganizationService service = factory.CreateOrganizationService(context.UserId);
+            Helper helper = new Helper();
+
+
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            {
+                Entity entity = (Entity)context.InputParameters["Target"];
+                string message = context.MessageName;
+                decimal projectBudget = (decimal)entity["new_projectbudget"]; //The Project Budget
+
+                try
+                {
+
+                    if(message.Equals("Update"))
+                    {
+                        Entity preimage = context.PreEntityImages.Contains("Project") ? context.PreEntityImages["Project"] : null;
+                        var selection = (OptionSetValue)preimage["new_category"];
+                        int selectedValue = selection.Value;
+                        string category = "";
+                        category = this.getSelectedValue(selectedValue);
+                        Entity categoryEntity = helper.getCategory(category, service);
+                        decimal categoryBudget = (decimal)categoryEntity["new_categorybudget"];
+                        if (projectBudget > categoryBudget)
+                        {
+                            throw new InvalidPluginExecutionException("The Entered Budget is bigger than the Category Budget" + System.Environment.NewLine + "The budget must be under : " + categoryBudget);
+                        }
+
+
+                    }
+                    else if(message.Equals("Create"))
+                    {
+                        // GET 
+                        if(entity.Contains("new_category"))
+                        {
+                            var selection = (OptionSetValue)entity["new_category"];
+                            int selectedValue = selection.Value;
+                            string category = "";
+                            category = this.getSelectedValue(selectedValue);
+
+                            Entity categoryEntity = helper.getCategory(category,service);
+                            decimal categoryBudget = (decimal)categoryEntity["new_categorybudget"];
+                            if(projectBudget > categoryBudget)
+                            {
+                                throw new InvalidPluginExecutionException("The Entered Budget is bigger than the Category Budget"+ System.Environment.NewLine + "The budget must be under : " + categoryBudget);
+                            }
+                        }
+                       
+                    }
+                   
+                }
+
+                catch (Exception ex)
+                {
+                    throw new InvalidPluginExecutionException(ex.Message);
+                }
+            }
+        }
+    }
+}
