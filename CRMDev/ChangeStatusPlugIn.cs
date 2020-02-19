422ad6c4609b697fb5649be09561bc3061ef7c39
Albi Goxhaj <albi.goxhaj@moreone.tech>
2020-02-19 16:26:07 +0100

    Update

---
+++ CRMDev/ChangeStatusPlugIn.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRMDev
+{
+    public class ChangeStatusPlugIn : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            IOrganizationServiceFactory factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+
+            IOrganizationService service = factory.CreateOrganizationService(context.UserId);
+            Helper helper = new Helper();
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            {
+
+            }
+        }
+    }
+}
